66d792a820636d91a93f3f5b160cf9f4109bdfac
Stan Bertrand <sbertrand@witekio.com>
2019-09-05 17:17:36 -0700

    SBFE : encrypted partition support - Storage
    This example can be tested devices regardless of flash encryption status.
    
    The following changes to the idf are needed :
    
    diff --git a/components/spi_flash/partition.c b/components/spi_flash/partition.c
    index 14de32c..46822d0 100644
    --- a/components/spi_flash/partition.c
    +++ b/components/spi_flash/partition.c
    @@ -333,7 +333,7 @@ esp_err_t esp_partition_read(const esp_partition_t* partition,
             return spi_flash_read(partition->address + src_offset, dst, size);
     #endif // CONFIG_SPI_FLASH_USE_LEGACY_IMPL
         } else {
    -#if CONFIG_SECURE_FLASH_ENC_ENABLED
    +#if CONFIG_SECURE_FLASH_ENC_ENABLED || 1
             if (partition->flash_chip != esp_flash_default_chip) {
                 return ESP_ERR_NOT_SUPPORTED;
             }
    @@ -375,7 +375,7 @@ esp_err_t esp_partition_write(const esp_partition_t* partition,
             return spi_flash_write(dst_offset, src, size);
     #endif // CONFIG_SPI_FLASH_USE_LEGACY_IMPL
         } else {
    -#if CONFIG_SECURE_FLASH_ENC_ENABLED
    +#if CONFIG_SECURE_FLASH_ENC_ENABLED || 1
             if (partition->flash_chip != esp_flash_default_chip) {
                 return ESP_ERR_NOT_SUPPORTED;
             }

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -7,11 +7,55 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "soc/efuse_reg.h"
+#include "esp_efuse.h"
+#include "esp_partition.h"
+#include "esp_flash_encrypt.h"
+#include "esp_efuse_table.h"
+
+static const char* TAG = "HelloWorld";
+
+static void example_print_flash_encryption_status(void)
+{
+    uint32_t flash_crypt_cnt = 0;
+    esp_efuse_read_field_blob(ESP_EFUSE_FLASH_CRYPT_CNT, &flash_crypt_cnt, 7);
+    printf("FLASH_CRYPT_CNT eFuse value is %d\n", flash_crypt_cnt);
+
+    esp_flash_enc_mode_t mode = esp_get_flash_encryption_mode();
+    if (mode == ESP_FLASH_ENC_MODE_DISABLED) {
+        printf("Flash encryption feature is disabled\n");
+    } else {
+        printf("Flash encryption feature is enabled in %s mode\n",
+            mode == ESP_FLASH_ENC_MODE_DEVELOPMENT ? "DEVELOPMENT" : "RELEASE");
+    }
+}
+
+
+static void example_read_flash(void)
+{
+    const esp_partition_t* partition = esp_partition_find_first(
+        ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_ANY, "storage");
+    assert(partition);
+
+    const size_t data_size = 64;
+
+    uint8_t read_data[data_size];
+    memset(read_data,0,data_size);
+    printf("Reading with esp_partition_read:\n");
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_partition_read(partition, 0, read_data, data_size));
+    ESP_LOG_BUFFER_HEXDUMP(TAG, read_data, data_size, ESP_LOG_INFO);
+
+    memset(read_data,0,data_size);
+    printf("Reading with spi_flash_read:\n");
+    ESP_ERROR_CHECK_WITHOUT_ABORT(spi_flash_read(partition->address, read_data, data_size));
+    ESP_LOG_BUFFER_HEXDUMP(TAG, read_data, data_size, ESP_LOG_INFO);
+}
 
 void app_main(void)
 {
@@ -29,6 +73,10 @@ void app_main(void)
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    example_print_flash_encryption_status();
+
+    example_read_flash();
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
